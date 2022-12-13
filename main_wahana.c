1dbf11208a9d6304a15f4362be8045269cd8d269
intandw1s <intandwisukma2004@gmail.com>
2022-12-13 21:40:50 +0800

    upload main_wahana
    upload main_wahana

---
+++ main_wahana.c
@@ -0,0 +1,19 @@
+//Pemesanan Tiket Wahana Hiburan
+//Ni Putu Adelia Candra Swari 2205551017
+//Ni Made Intan Dwi Sukma 2205551031
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "login.c"
+
+//MAIN
+int main(){
+    system("COLOR 71");
+    masuk();
+    system("pause");
+    return 0;
+}
+
